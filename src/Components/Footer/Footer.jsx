cef511e5c575a55f169457f5d2850f49cd31bd43
CaptainKhan <agaxanist@gmail.com>
2023-04-25 14:00:39 +0400

    footer fixed

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,23 +1,25 @@
-import React from 'react'
-import logo from '../../assets/images/logo.png'
+import React from "react";
+import logo from "../../assets/images/logo.png";
 
 const Footer = () => {
-    const year = new Date().getFullYear()
-    return (
+  const year = new Date().getFullYear();
+  return (
+    <div>
+      <div className="flex items-center justify-center">
+        <hr className="h-px w-4/5 bg-gray-400 opacity-50 outline-none border-none" />
+      </div>
+      <div className="flex items-center justify-around pt-4">
         <div>
-            <div className='flex items-center justify-center'>
-                <hr className='h-px w-4/5 bg-gray-400 opacity-50 outlined border-none' />
-            </div>
-            <div className='flex items-center justify-around pt-4'>
-                <img className='h-20' src={logo} alt="logo"></img>
-            </div>
-            <div>
-                <p className='text-black text-sm font-inter leading-none tracking-normal no-underline '>
-                    Copyright &copy; {year} page by Tarzan
-                </p>
-            </div>
+          <img className="h-20" src={logo} alt="logo"></img>
         </div>
-    )
-}
+        <div>
+          <p className="text-black text-sm font-inter no-underline normal-case">
+            Copyright {year} page by Marko Web
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
