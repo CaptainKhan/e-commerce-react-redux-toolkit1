88f96870abb73d5eadf00c5c6295fae83d377310
CaptainKhan <agaxanist@gmail.com>
2023-04-26 16:00:19 +0400

    project finished(for me i think. there's nothing to do)

--- src/Components/FilteredProducts/FilteredProducts.jsx
+++ src/Components/FilteredProducts/FilteredProducts.jsx
@@ -1,23 +1,45 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
 import { useParams } from 'react-router-dom'
 import ProductCard from './ProductCard';
 import { Button } from '@material-tailwind/react'
+import { useSelector, useDispatch } from 'react-redux';
 import {
     Menu,
     MenuHandler,
     MenuList,
     MenuItem,
 } from "@material-tailwind/react";
+import Error from '../Error/Error';
+import {
+    filterProducts,
+    filterGender,
+    sortByPrice,
+    filterByColor,
+    filterBySize
+} from '../../features/slices/productsSlice'
 
 
 export const FilteredProducts = () => {
 
     const products = useSelector(state => state.products.filteredProducts);
+    const error = useSelector(state => state.products.error);
     const { type } = useParams();
 
     const genderButtons = ['male', 'female'];
+    const colorButtons = ["red",
+        "green",
+        "purple",
+        "yellow",
+        "orange",
+        "blue",
+        "black",
+        "white",
+        "orange",
+        "brown"
+    ]
+    const sizeButtons = ["XS", "S", "M", "L", "XL", "XXL"]
 
+    const dispatch = useDispatch();
 
     return (
         <div>
@@ -37,6 +59,7 @@ export const FilteredProducts = () => {
                                             variant='outlined'
                                             ripple={true}
                                             className='text-black hover:bg-gray-300 duration-300 ease-in-out mr-4'
+                                            onClick={() => dispatch(filterGender(item))}
                                         >
                                             {item}
                                         </Button>
@@ -49,6 +72,7 @@ export const FilteredProducts = () => {
                                 variant='outlined'
                                 ripple={true}
                                 className='text-black hover:bg-gray-300 duration-300 ease-in-out mr-4'
+                                onClick={() => dispatch(sortByPrice())}
                             >
                                 High Price
                             </Button>
@@ -60,36 +84,91 @@ export const FilteredProducts = () => {
                                         variant='outlined'
                                         ripple={true}
                                         className='text-black hover:bg-gray-300 duration-300 ease-in-out mr-4'
+                                        onClick={() => dispatch(filterByColor())}
+
                                     >
                                         Select a Color
                                     </Button>
                                 </MenuHandler>
                                 <MenuList>
-                                    <MenuItem>Menu Item 1</MenuItem>
-                                    <MenuItem>Menu Item 2</MenuItem>
-                                    <MenuItem>Menu Item 3</MenuItem>
+                                    {colorButtons.map((item, index) => {
+                                        return (
+                                            <MenuItem
+                                                style={{ color: item }}
+                                                key={index}
+                                                onClick={() => dispatch(filterByColor(item))}
+                                            >
+                                                {item}
+                                            </MenuItem>
+                                        )
+                                    }
+                                    )}
+                                </MenuList>
+                            </Menu>
+                            <Menu>
+                                <MenuHandler>
+                                    <Button
+                                        color='gray'
+                                        size='lg'
+                                        variant='outlined'
+                                        ripple={true}
+                                        className='text-black hover:bg-gray-300 duration-300 ease-in-out mr-4'
+                                        disabled={type === "Bags" || type === "Shoes"}
+                                    >
+                                        Select a Size
+                                    </Button>
+                                </MenuHandler>
+                                <MenuList>
+                                    {sizeButtons.map((item, index) => {
+                                        return (
+                                            <MenuItem
+                                                onClick={() => dispatch(filterBySize(item))}
+                                                key={index}
+                                            >
+                                                {item}
+                                            </MenuItem>
+                                        )
+                                    })}
                                 </MenuList>
                             </Menu>
                         </div>
+                        <div className='pr-14'>
+                            <Button
+                                color='gray'
+                                size='lg'
+                                variant='outlined'
+                                ripple={true}
+                                className='text-black hover:bg-gray-300 duration-300 ease-in-out mr-4'
+                                onClick={() => dispatch(filterProducts(type))}
+                            >
+                                Clear Filter
+                            </Button>
+                        </div>
                     </div>
                 </div>
-                <div className='grid grid-cols-4 justify-items-center py-8 gap-12'>
-                    {products.filter((product) => product.type === type).map((product, index) => {
-                        return (
-                            <div key={index}>
-                                <ProductCard
-                                    id={product.id}
-                                    name={product.name}
-                                    text={product.text}
-                                    img={product.img}
-                                    price={product.price}
-                                    colors={product.color}
-                                >
-                                </ProductCard>
-                            </div>
-                        )
-                    })}
-                </div>
+                {error ? (
+                    <Error />
+                )
+                    :
+                    (
+                        <div className='grid grid-cols-4 justify-items-center py-8 gap-12'>
+                            {products.filter((product) => product.type === type).map((product, index) => {
+                                return (
+                                    <div key={index}>
+                                        <ProductCard
+                                            id={product.id}
+                                            name={product.name}
+                                            text={product.text}
+                                            img={product.img}
+                                            price={product.price}
+                                            colors={product.color}
+                                        >
+                                        </ProductCard>
+                                    </div>
+                                )
+                            })}
+                        </div>
+                    )}
             </div>
         </div>
     )
--- src/features/slices/productsSlice.jsx
+++ src/features/slices/productsSlice.jsx
@@ -5,16 +5,20 @@ import { storeData } from '../../assets/data/dummyData'
 export const productsSlice = createSlice({
     name: 'products',
     initialState: {
-        filteredProducts: JSON.parse(sessionStorage.getItem('filteredData')) || storeData,
-        singleProduct: JSON.parse(sessionStorage.getItem('oneProduct')) || storeData,
+        filteredProducts:
+            JSON.parse(sessionStorage.getItem('filteredData')) || storeData,
+        singleProduct:
+            JSON.parse(sessionStorage.getItem('oneProduct')) || storeData,
+        error: false,
     },
     reducers: {
         filterProducts: (state, action) => {
             try {
-                const filter = storeData.filter((product) => product.type === action.payload
+                const filter = storeData.filter(
+                    (product) => product.type === action.payload
                 );
                 state.filteredProducts = filter;
-                console.log('filter', filter);
+                state.error = false;
                 const saveState = JSON.stringify(filter);
                 sessionStorage.setItem('filteredData', saveState);
             }
@@ -22,6 +26,7 @@ export const productsSlice = createSlice({
                 return err;
             }
         },
+
         singleProduct(state, action) {
             try {
                 const oneProduct = storeData.filter((product) => product.id === action.payload);
@@ -33,8 +38,96 @@ export const productsSlice = createSlice({
                 return err;
             }
         },
+
+        filterGender(state, action) {
+            try {
+                const gender = state.filteredProducts.filter(
+                    (product) => product.gender === action.payload
+                )
+                state.error = false;
+                state.filteredProducts = gender;
+                const offGenderType = gender.length > 0;
+                if (offGenderType) {
+                    state.error = false;
+                    const saveState = JSON.stringify(gender);
+                    sessionStorage.setItem('filteredData', saveState);
+                } else {
+                    state.error = true;
+                    state.filteredProducts = []
+                }
+            } catch (err) {
+                return err
+            }
+        },
+
+        sortByPrice(state) {
+            try {
+                const price = state.filteredProducts.sort((a, b) =>
+                    a.price > b.price ? -1 : 1
+                )
+                state.filteredProducts = price;
+                let count = price.length;
+                if (count > 1) {
+                    const noError = false;
+                    state.error = noError;
+                    if (!noError) {
+                        state.filteredProducts = price;
+                        const saveState = JSON.stringify(price);
+                        sessionStorage.setItem("filteredData", saveState);
+                    } else {
+                        state.error = true;
+                        state.filteredProducts = []
+                    }
+                }
+            } catch (err) {
+                return err
+            }
+        },
+
+        filterByColor(state, action) {
+            try {
+                const color = state.filteredProducts.filter(
+                    (product) => product.color.includes(action.payload)
+                    // (product) => product.type === action.payload
+                )
+                state.error = false;
+                state.filteredProducts = color;
+                if (color.length <= 0) {
+                    state.error = true;
+                    state.filteredProducts = []
+                } else {
+                    state.error = false;
+                    state.filteredProducts = color;
+                    const saveState = JSON.stringify(color);
+                    sessionStorage.setItem('filteredData', saveState);
+                }
+            } catch (err) {
+                return err
+            }
+        },
+
+        filterBySize(state, action) {
+            try {
+                const size = state.filteredProducts.filter(
+                    (product) => product.size.includes(action.payload)
+                )
+                state.error = false;
+                state.filteredProducts = size;
+                if (size.length <= 0) {
+                    state.error = true;
+                    state.filteredProducts = []
+                } else {
+                    state.error = false;
+                    state.filteredProducts = size;
+                    const saveState = JSON.stringify(size);
+                    sessionStorage.setItem('filteredData', saveState);
+                }
+            } catch (err) {
+                return err
+            }
+        }
     },
 });
 
-export const { filterProducts, singleProduct } = productsSlice.actions;
+export const { filterProducts, singleProduct, filterGender, sortByPrice, filterByColor, filterBySize } = productsSlice.actions;
 export default productsSlice.reducer;
